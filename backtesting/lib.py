BUG: More robust index computation in `lib.resample_apply`
Fixes https://github.com/kernc/backtesting.py/issues/19

backtesting/lib.py
@@ -221,8 +221,10 @@ def resample_apply(rule: str,
 
     # Resample back to data index
     def wrap_func(resampled, *args, **kwargs):
-        ind = func(resampled, *args, **kwargs)
-        ind = ind.reindex(index=series.index | ind.index,
+        ind = pd.Series(np.asarray(func(resampled, *args, **kwargs)),
+                        index=resampled.index,
+                        name=resampled.name)
+        ind = ind.reindex(index=series.index | resampled.index,
                           method='ffill').reindex(series.index)
         return ind
 