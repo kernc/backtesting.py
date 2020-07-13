8bd7edf1cbfebbde56c0df730846943d2f0360e4
Kernc <kerncece@gmail.com>
2020-07-13 17:43:50 +0200

    MNT: Avoid testing via deprecated setup.py test_suite

--- backtesting/test/__main__.py
+++ backtesting/test/__main__.py
@@ -3,4 +3,4 @@ import unittest
 suite = unittest.defaultTestLoader.discover('backtesting.test',
                                             pattern='_test*.py')
 if __name__ == '__main__':
-    unittest.TextTestRunner().run(suite)
+    unittest.TextTestRunner(verbosity=2).run(suite)
