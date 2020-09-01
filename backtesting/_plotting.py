f29d27db0284eaa09970ffe59b153cabfbc68ec9
Kernc <kerncece@gmail.com>
2020-09-02 01:03:14 +0200

    BUG: Fix Bokeh tooltip showing literal '&nbsp;'
    Noticed with Bokeh 2.2.1

--- backtesting/_plotting.py
+++ backtesting/_plotting.py
@@ -234,7 +234,7 @@ this.labels = this.labels || formatter.doFormat(ticks
 return this.labels[index] || "";
         ''')
 
-    NBSP = '&nbsp;' * 4
+    NBSP = '\N{NBSP}' * 4
     ohlc_extreme_values = df[['High', 'Low']].copy(deep=False)
     ohlc_tooltips = [
         ('x, y', NBSP.join(('$index',
