c6140cb9902f288b9fb7ae8a667ee524c04376b8
Kernc <kerncece@gmail.com>
2020-09-22 01:24:07 +0200

    DOC: Update FAQ link

--- backtesting/__init__.py
+++ backtesting/__init__.py
@@ -30,8 +30,8 @@ urlpath=lab%2Ftree%2Fdoc%2Fexamples%2FQuick%20Start%20User%20Guide.ipynb
 
 ## FAQ
 
-Potentially outdated answers to popular questions can be found on the
-[issue tracker](https://github.com/kernc/backtesting.py/issues?q=label%3Aquestion).
+Potentially outdated answers to frequent and popular questions can be found on the
+[issue tracker](https://github.com/kernc/backtesting.py/issues?q=label%3Aquestion+-label%3Ainvalid).
 
 ## License
 
