a86f348cae35a128fd35c65dbe6177e89269d69c
Kernc <kerncece@gmail.com>
2020-11-14 17:23:29 +0100

    MNT: Fix CI test suite

--- backtesting/test/__main__.py
+++ backtesting/test/__main__.py
@@ -1,6 +1,8 @@
+import sys
 import unittest
 
 suite = unittest.defaultTestLoader.discover('backtesting.test',
                                             pattern='_test*.py')
 if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    result = unittest.TextTestRunner(verbosity=2).run(suite)
+    sys.exit(not result.wasSuccessful())
