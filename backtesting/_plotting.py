50fd012deaec983604d4a9c99eb40ee1d75aa35b
Zeel Patel <zlpatel@users.noreply.github.com>
2021-05-22 19:30:20 -0700

    Auto-scale Y-axis for indicators when zooming #356

--- backtesting/_plotting.py
+++ backtesting/_plotting.py
@@ -523,6 +523,10 @@ return this.labels[index] || "";
             colors = colors and cycle(_as_list(colors)) or (
                 cycle([next(ohlc_colors)]) if is_overlay else colorgen())
             legend_label = LegendStr(value.name)
+            indicator_max = value.df.max(axis='columns')
+            indicator_min = value.df.min(axis='columns')
+            source.add(indicator_max, f'indicator_{i}_range_max')
+            source.add(indicator_min, f'indicator_{i}_range_min')
             for j, arr in enumerate(value, 1):
                 color = next(colors)
                 source_name = f'{legend_label}_{i}_{j}'
@@ -584,7 +588,8 @@ return this.labels[index] || "";
         figs_above_ohlc.append(_plot_drawdown_section())
 
     if plot_pl:
-        figs_above_ohlc.append(_plot_pl_section())
+        fig_pl = _plot_pl_section()
+        figs_above_ohlc.append(fig_pl)
 
     if plot_volume:
         fig_volume = _plot_volume_section()
@@ -607,9 +612,15 @@ return this.labels[index] || "";
 
     custom_js_args = dict(ohlc_range=fig_ohlc.y_range,
                           source=source)
+    if plot_pl:
+        custom_js_args.update(pl_range=fig_pl.y_range)
     if plot_volume:
         custom_js_args.update(volume_range=fig_volume.y_range)
-
+    indicator_ranges = {}
+    for idx, indicator in enumerate(indicator_figs):
+        indicator_range_key = f'indicator_{idx}_range'
+        indicator_ranges.update({indicator_range_key: indicator.y_range})
+    custom_js_args.update({'indicator_ranges': indicator_ranges})
     fig_ohlc.x_range.js_on_change('end', CustomJS(args=custom_js_args,
                                                   code=_AUTOSCALE_JS_CALLBACK))
 
