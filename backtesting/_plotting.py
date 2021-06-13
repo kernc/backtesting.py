97712c7b91ab5fe9520010a669b5e1aabe9e62fc
Zeel Patel <zlpatel@users.noreply.github.com>
2021-06-13 12:20:08 -0700

    Fixing build error by adding [:] kernc#356

--- backtesting/_plotting.py
+++ backtesting/_plotting.py
@@ -392,7 +392,7 @@ return this.labels[index] || "";
         size = trades['Size'].abs()
         size = np.interp(size, (size.min(), size.max()), (8, 20))
         ohlcv_index_trade_close_arr = np.empty(source.data['index'][-1]+1, dtype=float)
-        ohlcv_index_trade_close_arr[trade_source.data['index']] = trades['ReturnPct']
+        ohlcv_index_trade_close_arr[trade_source.data['index'][:]] = trades['ReturnPct']
         source.add(ohlcv_index_trade_close_arr, 'return_pct')
         trade_source.add(returns_long, 'returns_long')
         trade_source.add(returns_short, 'returns_short')
