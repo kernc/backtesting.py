abaf11b0f2056341167d273e13a6793e6467eea4
Shuai Sun <shuaisun@Shuais-iMac.local>
2022-04-20 11:31:33 +0800

    trial test

---
+++ sample.py
@@ -0,0 +1,24 @@
+from backtesting import Backtest, Strategy
+from backtesting.lib import crossover
+
+from backtesting.test import SMA, GOOG
+
+
+class SmaCross(Strategy):
+    def init(self):
+        price = self.data.Close
+        self.ma1 = self.I(SMA, price, 10)
+        self.ma2 = self.I(SMA, price, 20)
+
+    def next(self):
+        if crossover(self.ma1, self.ma2):
+            self.buy()
+        elif crossover(self.ma2, self.ma1):
+            self.sell()
+
+
+if __name__ == '__main__':
+    bt = Backtest(AAPL, SmaCross, commission=.002,
+                  exclusive_orders=True)
+    stats = bt.run()
+    bt.plot()
