a8082f14fa3ad2812144dbf9c83de7ebff309b5a
Kernc <kerncece@gmail.com>
2022-12-05 18:58:35 +0100

    Add Tag column to stats._trades

--- backtesting/_stats.py
+++ backtesting/_stats.py
@@ -65,6 +65,7 @@ def compute_stats(
             'ReturnPct': [t.pl_pct for t in trades],
             'EntryTime': [t.entry_time for t in trades],
             'ExitTime': [t.exit_time for t in trades],
+            'Tag': [t.tag for t in trades],
         })
         trades_df['Duration'] = trades_df['ExitTime'] - trades_df['EntryTime']
     del trades
