b4f4b693272a2c3c338ed75aba6f8c17321bb415
ali gheshlaghi <aliigheshlaghi97@gmail.com>
2023-08-07 01:49:02 +0330

    Added TP and SL into stats file.

--- backtesting/_stats.py
+++ backtesting/_stats.py
@@ -65,6 +65,8 @@ def compute_stats(
             'ReturnPct': [t.pl_pct for t in trades],
             'EntryTime': [t.entry_time for t in trades],
             'ExitTime': [t.exit_time for t in trades],
+            'SL': [t.sl for t in trades],
+            'TP': [t.tp for t in trades],
             'Tag': [t.tag for t in trades],
         })
         trades_df['Duration'] = trades_df['ExitTime'] - trades_df['EntryTime']
