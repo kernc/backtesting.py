21907e98b59d6c5c858134a5352826ee3fb7c494
Kernc <kerncece@gmail.com>
2025-02-03 21:45:50 +0100

    TST: Cli entrypoint backtesting.test takes unittest argv

--- backtesting/test/__main__.py
+++ backtesting/test/__main__.py
@@ -1,8 +1,8 @@
-import sys
 import unittest
 
 suite = unittest.defaultTestLoader.discover('backtesting.test',
                                             pattern='_test*.py')
+unittest.defaultTestLoader.suiteClass = lambda _: suite
+
 if __name__ == '__main__':
-    result = unittest.TextTestRunner(verbosity=2).run(suite)
-    sys.exit(not result.wasSuccessful())
+    unittest.main(verbosity=2)
