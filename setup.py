4d1ffa2f15614a4069a7eac273e4804fd93bf1d1
Kernc <kerncece@gmail.com>
2025-02-06 07:36:59 +0100

    MNT: Pin bokeh != 3.2.*
    Avoid error "gridplot got multiple values for keyword argument 'logo'".
    
    Refs: https://github.com/bokeh/bokeh/issues/13369

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ if __name__ == '__main__':
         install_requires=[
             'numpy >= 1.17.0',
             'pandas >= 0.25.0, != 0.25.0',
-            'bokeh >= 1.4.0, != 3.0.*',
+            'bokeh >= 1.4.0, != 3.0.*, != 3.2.*',
             'joblib',
         ],
         extras_require={
